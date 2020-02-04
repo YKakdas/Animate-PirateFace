77c601f945bc52054d37b0022800c09618c5dda5
Yasar <ykakdas@gmail.com>
2020-02-04 23:03:02 +0300

    Template for drawing circle

--- PirateFace.cpp
+++ PirateFace.cpp
@@ -1,20 +1,128 @@
-// PirateFace.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Angel.h"
+#include <Math.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace Angel;
+
+typedef vec4 color4;
+typedef vec4 point4;
+
+const int NumVertices = 93;
+const double PI = 3.141592653589793238463;
+
+const GLint width = 500;
+const GLint height = 500;
+
+point4 points[NumVertices];
+color4 colors[NumVertices];
+
+point4 circle[30];
+color4 color = { 1.0,0.0,0.0,1.0 };
+point4 center = { 0.0,0.0,0.0,1.0 };
+
+GLfloat radius = 0.3;
+
+/* vertices of LShape
+	1---2
+	 - -
+	 - -
+	 - -3
+	 -------------4
+	0-------------5
+*/
+
+int Index = 0;
+
+void triangle(int b, int c) {
+	colors[Index] = color; points[Index] = center; Index++;
+	colors[Index] = color; points[Index] = circle[b %30]; Index++;
+	colors[Index] = color; points[Index] = circle[c%30]; Index++;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void fill() {
+	for (int i = 0; i < 30; i++)
+	{
+		float angle = 2 * PI * i / 30;
+		point4 point;
+		point.x = center.x + cos(angle) * radius;
+		point.y = center.y + sin(angle) * radius;
+		point.z = 0.6;
+		circle[i] = point;
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (int i = 0; i <= 29; i++) {
+		triangle(i , i + 1);
+	}
+}
+
+void init()
+{
+	fill();
+
+	// Create a vertex array object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// Create and initialize a buffer object
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors),
+		NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(points), points);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors);
+
+	// Load shaders and use the resulting shader program
+	GLuint program = InitShader("vshader.glsl", "fshader.glsl");
+	glUseProgram(program);
+
+	// set up vertex arrays
+	GLuint vPosition = glGetAttribLocation(program, "vPosition");
+	glEnableVertexAttribArray(vPosition);
+	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0,
+		BUFFER_OFFSET(0));
+
+	GLuint vColor = glGetAttribLocation(program, "vColor");
+	glEnableVertexAttribArray(vColor);
+	glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0,
+		BUFFER_OFFSET(sizeof(points)));
+
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+
+	glutSwapBuffers();
+}
+
+void myMouse(int btn, int state, int x, int y) {
+	
+}
+
+void myKeyboard(unsigned char key, int x, int y) {
+	
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(width, height);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("LShape");
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	init();
+
+	glutKeyboardFunc(myKeyboard);
+	glutMouseFunc(myMouse);
+	glutDisplayFunc(myDisplay);
+	glutMainLoop();
+	return 0;
+}
